aa44c7610b7d349804e272426dcf46308959ff37
Lxp <Lxp@Lxp>
2019-02-18 14:59:17 +0800

    添加了主页和登录页面的配置

--- JingBao/src/cn/zkingsoft/web/action/RegisterAction.java
+++ JingBao/src/cn/zkingsoft/web/action/RegisterAction.java
@@ -6,8 +6,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import cn.zkingsoft.service.UserService;
-import cn.zkingsoft.service.impl.UserServiceImpl;
 import cn.zkingsoft.web.core.ActionForm;
 import cn.zkingsoft.web.core.ActionForward;
 import cn.zkingsoft.web.core.DispatcherAction;
@@ -22,12 +20,6 @@ public class RegisterAction extends DispatcherAction{
 	
 	public ActionForward zhuce(HttpServletRequest req, HttpServletResponse resp, ActionForm form)
 			throws ServletException, IOException {
-		RegisterForm rf = (RegisterForm)form;
-		if(rf!=null){
-			//req.getSession().setAttribute("user", rf);
 			return new ActionForward(true,"loginandregisterhtml");
-		}else{
-			return new ActionForward(true,"loginandregisterhtml");
-		}
 	}
 }
