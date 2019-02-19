d38818d5f785f7584d755823f46fb341b8870129
qdl <qdl@QDL-PC>
2019-02-19 13:08:10 +0800

    订单pojo

--- JingBao/src/cn/zkingsoft/pojo/Dingdan.java
+++ JingBao/src/cn/zkingsoft/pojo/Dingdan.java
@@ -3,105 +3,68 @@ package cn.zkingsoft.pojo;
 import java.io.Serializable;
 
 public class Dingdan implements Serializable{
+	private String did;
 	private String uid;
 	private String pid;
-	private String pname;
-	private String category;//品牌
-	private String color;//颜色
-	private Double kucun;//库存
-	private Integer dnum;//订单数量
-	private Double dprice;//订单价格
-	private Integer dno;//订单状态
-	
+	private Integer dnum;
+	private Integer dno;
+	private String address;
+	private String discount;
 	public Dingdan() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
-	public Dingdan(String uid, String pid, String pname, String category, String color, Double kucun, Integer dnum,
-			Double dprice, Integer dno) {
+	public Dingdan(String did, String uid, String pid, Integer dnum, Integer dno, String address, String discount) {
 		super();
+		this.did = did;
 		this.uid = uid;
 		this.pid = pid;
-		this.pname = pname;
-		this.category = category;
-		this.color = color;
-		this.kucun = kucun;
 		this.dnum = dnum;
-		this.dprice = dprice;
 		this.dno = dno;
+		this.address = address;
+		this.discount = discount;
 	}
-
+	public String getDid() {
+		return did;
+	}
+	public void setDid(String did) {
+		this.did = did;
+	}
 	public String getUid() {
 		return uid;
 	}
-
 	public void setUid(String uid) {
 		this.uid = uid;
 	}
-
 	public String getPid() {
 		return pid;
 	}
-
 	public void setPid(String pid) {
 		this.pid = pid;
 	}
-
-	public String getPname() {
-		return pname;
-	}
-
-	public void setPname(String pname) {
-		this.pname = pname;
-	}
-
-	public String getCategory() {
-		return category;
-	}
-
-	public void setCategory(String category) {
-		this.category = category;
-	}
-
-	public String getColor() {
-		return color;
-	}
-
-	public void setColor(String color) {
-		this.color = color;
-	}
-
-	public Double getKucun() {
-		return kucun;
-	}
-
-	public void setKucun(Double kucun) {
-		this.kucun = kucun;
-	}
-
 	public Integer getDnum() {
 		return dnum;
 	}
-
 	public void setDnum(Integer dnum) {
 		this.dnum = dnum;
 	}
-
-	public Double getDprice() {
-		return dprice;
-	}
-
-	public void setDprice(Double dprice) {
-		this.dprice = dprice;
-	}
-
 	public Integer getDno() {
 		return dno;
 	}
-
 	public void setDno(Integer dno) {
 		this.dno = dno;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDiscount() {
+		return discount;
+	}
+	public void setDiscount(String discount) {
+		this.discount = discount;
 	}
 	
 }
