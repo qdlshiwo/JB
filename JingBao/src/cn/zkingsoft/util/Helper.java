7f5bb10d9d094f2dd16e602a82c63edaaf3ec2a9
qdl <qdl@QDL-PC>
2019-02-19 13:49:37 +0800

    主键生成器

--- JingBao/src/cn/zkingsoft/util/Helper.java
+++ JingBao/src/cn/zkingsoft/util/Helper.java
@@ -1,5 +1,22 @@
 package cn.zkingsoft.util;
 
-public class Helper {
+import java.util.Random;
 
+public class Helper {
+	public  static String PrimaryKeyComplete(){
+		StringBuilder str=new StringBuilder();//定义变长字符串
+		Random random=new Random();
+		//随机生成数字，并添加到字符串
+		for(int i=0;i<8;i++){
+		    str.append(random.nextInt(10));
+		}
+		//将字符串转换为数字并输出
+		System.out.println("生成了"+str);
+		return new String(str);
+		
+	}
+	public static void main(String[] args) {
+		String s = PrimaryKeyComplete();
+		
+	}
 }
